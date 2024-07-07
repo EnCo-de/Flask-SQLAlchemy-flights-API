d8ae4f630e552f5bcd1db243f793272f803a9a5d
EnCo-de <142403662+EnCo-de@users.noreply.github.com>
2024-07-07 17:08:00 +0400

    Implement 2. airoperators

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,24 +2,13 @@ from app.api import bp
 # from app.extensions import db
 from app.models.flights import Flight
 from flask import jsonify
-# from sqlalchemy import and_
+from sqlalchemy import func # and_
 from datetime import datetime
 
 
 @bp.route('/')
 def index():
     return {'api': '200 OK'}
-
-@bp.route('air-operators/<airoperator_name>/', methods=['GET'])
-def airoperators(airoperator_name=None):
-    if airoperator_name:
-        data = Flight.query.filter_by(airoperator_name=airoperator_name)
-    else:
-        data = Flight.query.all()
-    print('\n\n', data)
-    if isinstance(data, Flight):
-        return data.to_json()
-    return [flight.to_json() for flight in data]
 
 @bp.route('permissions/<start>/<end>/', methods=['GET'])
 def permissions(start=None, end=None):
@@ -38,3 +27,17 @@ def permissions(start=None, end=None):
     #     return data.jq()
     # return [ta.jq() for ta in data]
     return jsonify({"permissions": [each.to_json() for each in data]})
+
+
+@bp.route('air-operators/<airoperator_name>/', methods=['GET'])
+def airoperators(airoperator_name):
+    # arrival_1_date_time
+    # departure_1_date_time
+    data = Flight.query.filter_by(airoperator_name=airoperator_name).filter(
+            func.time(Flight.departure_1_date_time).between("01:00","07:00"), 
+            func.time(Flight.arrival_1_date_time).between("01:00","07:00"))
+    # print('\n\n', data)
+    # if isinstance(data, Flight):
+    #     return data.to_json()
+    return [flight.to_json() for flight in data]
+
